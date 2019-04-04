5a85b85029b70c7121538f74832a23a09c20b467
PabloCaDa <pablo.cabreradarias@gmail.com>
2019-04-04 19:41:18 +0100

    Toolbar compoennt and favicon

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -4,7 +4,7 @@ import './Toolbar.css'
 const Toolbar = (props) => {
   return (
     <div className="toolbar">
-        <i class="fab fa-free-code-camp mr-2"></i> 
+        <i class="fab fa-free-code-camp mr-2"></i>
             {props.title}
         <i className="fas fa-arrows-alt fullScreen__icon"></i>
     </div>
